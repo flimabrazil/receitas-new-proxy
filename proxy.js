f5b4128a24ef68229b7a7e228236ecbd656db01e
Flavio Lima <flima@Diver-iMac.local>
2025-06-27 18:07:52 -0300

    Proxy final funcionando com CORS

--- proxy.js
+++ proxy.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-app.use(cors());
 const fetch = require('node-fetch');
+
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Proxy para TheMealDB
